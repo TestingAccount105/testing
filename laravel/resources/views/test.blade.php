b3ef640acd14ce0e0efa38a95e9b28a0095a8999
NandoG1 <fernandogunawan291105@gmail.com>
2025-03-08 08:41:28 +0700

    Firt CoMMIT

---
+++ laravel/resources/views/test.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+        <h1 class="text-2xl font-bold text-gray-800">Hello, Laravel + Tailwind!</h1>
+        <p class="text-gray-600 font-bold">Ayo buat UI yang keren dengan Tailwind CSS.</p>
+        <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 hover:px-8">Klik Aku</button>
+        <p class="p-[30px]">Cara dalam memproses informasi adalah dengan menambahkan berbagai cara kita ke dalam dataset</p>
+    </div>
+</body>
+</html>
